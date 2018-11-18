4d48781fca2550a746137c6f55c3898941707b57
Geidel Xavier <geidel.solivan@upr.edu>
2018-11-17 21:47:52 -0800

    Added money armor and recipes with temporary color texture

--- src/main/java/com/tuxedosteve/JP3/init/ModItems.java
+++ src/main/java/com/tuxedosteve/JP3/init/ModItems.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.tuxedosteve.JP3.items.ItemBase;
+import com.tuxedosteve.JP3.items.armor.ArmorBase;
 import com.tuxedosteve.JP3.items.tools.ToolAxe;
 import com.tuxedosteve.JP3.items.tools.ToolHoe;
 import com.tuxedosteve.JP3.items.tools.ToolPickaxe;
 import com.tuxedosteve.JP3.items.tools.ToolSpade;
 import com.tuxedosteve.JP3.items.tools.ToolSword;
+import com.tuxedosteve.JP3.util.Reference;
 
+import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemHoe;
 import net.minecraft.item.ItemPickaxe;
@@ -25,6 +30,8 @@ public class ModItems {
 	
 	//Materials
 	public static final ToolMaterial MATERIAL_MONEY = EnumHelper.addToolMaterial("material_money", 3, 250, 8.0F, 4.0F, 10);
+	public static final ArmorMaterial ARMOR_MATERIAL_MONEY = EnumHelper.addArmorMaterial("armor_material_money", Reference.MOD_ID +":money",6, 
+			new int[] {1,2,3,1}, 10, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F);
 	
 	
 	//Items
@@ -38,4 +45,11 @@ public class ModItems {
 	public static final ItemPickaxe MONEY_PICKAXE = new ToolPickaxe("money_pickaxe", MATERIAL_MONEY);
 	public static final ItemAxe MONEY_AXE = new ToolAxe("money_axe", MATERIAL_MONEY);
 	public static final ItemHoe MONEY_HOE = new ToolHoe("money_hoe", MATERIAL_MONEY);
+	
+	//Armor
+	public static final Item MONEY_HELMET = new ArmorBase("money_helmet",ARMOR_MATERIAL_MONEY,1,EntityEquipmentSlot.HEAD);
+	public static final Item MONEY_CHESTPLATE = new ArmorBase("money_chestplate",ARMOR_MATERIAL_MONEY,1,EntityEquipmentSlot.CHEST);
+	public static final Item MONEY_LEGGINGS = new ArmorBase("money_leggings",ARMOR_MATERIAL_MONEY,2,EntityEquipmentSlot.LEGS);
+	public static final Item MONEY_BOOTS = new ArmorBase("money_boots",ARMOR_MATERIAL_MONEY,1,EntityEquipmentSlot.FEET);
+	
 }
---
+++ src/main/java/com/tuxedosteve/JP3/items/armor/ArmorBase.java
@@ -0,0 +1,31 @@
+package com.tuxedosteve.JP3.items.armor;
+
+import com.tuxedosteve.JP3.Main;
+import com.tuxedosteve.JP3.init.ModItems;
+import com.tuxedosteve.JP3.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+
+public class ArmorBase extends ItemArmor implements IHasModel{
+
+	public ArmorBase(String name, ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
+		super(materialIn, renderIndexIn, equipmentSlotIn);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.COMBAT);
+
+		ModItems.ITEMS.add(this);
+		
+	}
+	
+	@Override
+	public void registerModels() {
+
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+
+	}
+
+
+}
